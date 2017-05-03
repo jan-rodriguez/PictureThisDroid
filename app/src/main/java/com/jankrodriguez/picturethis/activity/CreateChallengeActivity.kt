642b146df2fa491cfde0a4da8b4d95b9b0fc7b52
Jan <prdude241@gmail.com>
2017-05-03 10:02:27 -0700

    Updated dependencies. Colorized most things. Working on creating a challenge

--- app/src/main/java/com/jankrodriguez/picturethis/activity/CreateChallengeActivity.kt
+++ app/src/main/java/com/jankrodriguez/picturethis/activity/CreateChallengeActivity.kt
@@ -1,8 +1,7 @@
 package com.jankrodriguez.picturethis.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.support.v7.app.AppCompatActivity
 import com.jankrodriguez.picturethis.R
 
 class CreateChallengeActivity : AppCompatActivity() {
@@ -10,5 +9,7 @@ class CreateChallengeActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_create_challenge)
+
+		supportActionBar?.setTitle(R.string.create_challenge_title)
 	}
 }
